e046e8f3b92868d9a635ac313965507406d52295
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-15 16:26:54 +0200

    Topbar logo, phone number and email with icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,20 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons";
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">Logo</a>
+                    <a href="#intro" className="logo">logicCode.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+44 543 43 55</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>logicCode@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right"></div>
             </div>
