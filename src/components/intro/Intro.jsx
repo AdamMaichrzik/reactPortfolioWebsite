f07e6904d30f3c9d1d4fb62fcd4faef22c0d788e
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 12:23:44 +0200

    Intro left side img done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,11 @@ import "./intro.scss";
 export default function Intro() {
 	return (
 		<div className="intro" id="intro">
-			<div className="left"></div>
+			<div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt=""/>
+                </div>
+            </div>
 			<div className="right"></div>
 		</div>
 	);
