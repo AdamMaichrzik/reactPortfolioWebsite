cc1f9fa849e5986f1a95366570311e0a27cbb07c
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 17:22:09 +0200

    Testimonials card data connected

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -32,21 +32,22 @@ export default function Testimonials() {
 		<div className="testimonials" id="testimonials">
 			<h1>Testimonials</h1>
 			<div className="container">
-				<div className="card">
-					<div className="top">
-						<img src="assets/right-arrow.png" className="left " alt="" />
-						<img className="user" src="" alt="" />
-						<img className="right" src="assets/youtube.png" alt="" />
+				{data.map((d) => (
+					<div className={d.featured ? "card featured" : "card"}>
+						<div className="top">
+							<img src="assets/right-arrow.png" className="left " alt="" />
+							<img className="user" src={d.img} alt="" />
+							<img className="right" src={d.icon} alt="" />
+						</div>
+						<div className="center">
+							{d.desc}
+						</div>
+						<div className="bottom">
+							<h3>{d.name}</h3>
+							<h4>{d.title}</h4>
+						</div>
 					</div>
-					<div className="center">
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam,
-						dolore aperiam.
-					</div>
-					<div className="bottom">
-						<h3>Alex </h3>
-						<h4>Photographer</h4>
-					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
