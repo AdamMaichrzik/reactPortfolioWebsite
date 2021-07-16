7e32de45f2c7d8c5a37d44e5482d9c6501de6601
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 12:07:32 +0200

    Code cleaning

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,29 +1,31 @@
-import "./topbar.scss"
-import {Person, Mail} from "@material-ui/icons";
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar({menuOpen, setMenuOpen}) {
-    return (
-        <div className={"topbar " + (menuOpen && "active")}>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo">logicCode.</a>
-                    <div className="itemContainer">
-                        <Person className="icon"/>
-                        <span>+44 543 43 55</span>
-                    </div> 
-                    <div className="itemContainer">
-                        <Mail className="icon"/>
-                        <span>logicCode@gmail.com</span>
-                    </div>
-                </div>
-                <div className="right">
-                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+export default function Topbar({ menuOpen, setMenuOpen }) {
+	return (
+		<div className={"topbar " + (menuOpen && "active")}>
+			<div className="wrapper">
+				<div className="left">
+					<a href="#intro" className="logo">
+						logicCode.
+					</a>
+					<div className="itemContainer">
+						<Person className="icon" />
+						<span>+44 543 43 55</span>
+					</div>
+					<div className="itemContainer">
+						<Mail className="icon" />
+						<span>logicCode@gmail.com</span>
+					</div>
+				</div>
+				<div className="right">
+					<div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+						<span className="line1"></span>
+						<span className="line2"></span>
+						<span className="line3"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
