b9f7f9164fccc4e2203653bc67d4d7d07f3ce425
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 13:29:43 +0200

    Added projects to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,6 +16,26 @@ export default function Portfolio() {
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt=""/>
                     <h3>Banking App</h3>
                 </div>
+                <div className="item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
             </div>
         </div>
     )
