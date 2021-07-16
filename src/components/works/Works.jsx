3c5f799f58d02333bd1c692729eb17416e7634cf
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 16:39:22 +0200

    Works part working slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
+	const [currentSlide, setCurrentSlide] = useState(0);
 	const data = [
 		{
 			id: "1",
@@ -25,9 +27,18 @@ export default function Works() {
 		},
 	];
 
+	const handleClick = (way) => {
+		way === "left"
+			? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+			: setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+	};
+
 	return (
 		<div className="works" id="works">
-			<div className="slider">
+			<div
+				className="slider"
+				style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+			>
 				{data.map((d) => (
 					<div className="container">
 						<div className="item">
@@ -51,8 +62,18 @@ export default function Works() {
 					</div>
 				))}
 			</div>
-			<img src="assets/arrow.png" className="arrow left" alt="" />
-			<img src="assets/arrow.png" className="arrow right" alt="" />
+			<img
+				src="assets/arrow.png"
+				className="arrow left"
+				alt=""
+				onClick={() => handleClick("left")}
+			/>
+			<img
+				src="assets/arrow.png"
+				className="arrow right"
+				alt=""
+				onClick={() => handleClick()}
+			/>
 		</div>
 	);
 }
