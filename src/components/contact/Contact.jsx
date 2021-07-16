ad69b17e7972b3ec595671b0f64f080f1932f835
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 18:30:48 +0200

    Mobile responsive styling and code cleaning

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-    const [message, setMessage] = useState(false); 
+	const [message, setMessage] = useState(false);
 	const handleSubmit = (e) => {
-        e.preventDefault();
-        setMessage(true);
-    };
+		e.preventDefault();
+		setMessage(true);
+	};
 	return (
 		<div className="contact" id="contact">
 			<div className="left">
@@ -18,7 +18,7 @@ export default function Contact() {
 					<input type="text" placeholder="Email" name="" id="" />
 					<textarea placeholder="Message "></textarea>
 					<button type="submit">Send</button>
-                    {message && <span>Thanks! I will reply ASAP :) </span>}
+					{message && <span>Thanks! I will reply ASAP :) </span>}
 				</form>
 			</div>
 		</div>
