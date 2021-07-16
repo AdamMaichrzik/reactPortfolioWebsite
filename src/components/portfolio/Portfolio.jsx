75728fffdb84443580907a95413f9c6b2f5baa78
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 13:17:20 +0200

    portfolio title and some links styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,9 +3,9 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            <h1>Portfolip</h1>
+            <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
+                <li className="active">Featured</li>
                 <li>Web App</li>
                 <li>Mobile App</li>
                 <li>Design</li>
