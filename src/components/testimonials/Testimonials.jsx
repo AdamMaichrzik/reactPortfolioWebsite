36d10b3c3242d6377935a07c718aae387eb93bd9
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-16 17:15:57 +0200

    One testimonials card styling

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,20 +1,26 @@
-import "./testimonials.scss"
+import "./testimonials.scss";
 
 export default function Testimonials() {
-    return (
-        <div className="testimonials" id="testimonials">
-            <h1>Testimonials</h1>
-            <div className="container">
-                <div className="card">
-                    <div className="top">
-                        <img src="assets/right-arrow.png" className="left " alt=""/>
-                        <img src="" alt=""/>
-                        <img src="assets/youtube.png" alt=""/>
+	return (
+		<div className="testimonials" id="testimonials">
+			<h1>Testimonials</h1>
+			<div className="container">
+				<div className="card">
+					<div className="top">
+						<img src="assets/right-arrow.png" className="left " alt="" />
+						<img className="user" src="" alt="" />
+						<img className="right" src="assets/youtube.png" alt="" />
+					</div>
+					<div className="center">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam,
+						dolore aperiam.
+					</div>
+					<div className="bottom">
+                        <h3>Alex </h3>
+                        <h4>Photographer</h4>
                     </div>
-                    <div className="center"></div>
-                    <div className="bottom"></div>
-                </div>
-            </div>
-        </div>
-    )
+				</div>
+			</div>
+		</div>
+	);
 }
